dd050c4e8bfabdb678d1ad6b2561de77b50772be
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2024-02-19 15:00:01 -0300

    Create GET /contracts endpoint

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.set('models', sequelize.models)
  * Gets all the contracts for a given Client or Contractor
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
+app.get('/contracts/:id',getProfile, async (req, res) =>{
     const {Contract} = req.app.get('models')
     const {id} = req.params
     const {id: profileId} = req.profile
@@ -28,4 +28,19 @@ app.get('/contracts/:id',getProfile ,async (req, res) =>{
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
+
+// GET /contracts - Returns a list of contracts belonging to a user (client or contractor), the list should only contain non terminated contracts.
+app.get('/contracts', getProfile, async (req, res) => {
+    const {Contract} = req.app.get('models');
+    const {id: profileId} = req.profile;
+    const contracts = await Contract.findAll({where:
+    {
+        [Op.or]: [
+            {ContractorId: profileId},
+            {ClientId: profileId}
+        ]
+    }})
+    return res.json(contracts)
+})
+
 module.exports = app;
