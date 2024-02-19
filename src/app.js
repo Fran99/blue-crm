b888a671cd5e78a9e783882093975d1e59c8add1
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2024-02-19 14:38:30 -0300

    Fix GET /contracts:id security issue

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
+const {Op} = require("sequelize");
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
@@ -14,7 +15,16 @@ app.set('models', sequelize.models)
 app.get('/contracts/:id',getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
     const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
+    const {id: profileId} = req.profile
+    const contract = await Contract.findOne({
+        where: {
+            id,
+            [Op.or]: [
+                {ContractorId: profileId},
+                {ClientId: profileId}
+            ]
+        }
+    })
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
