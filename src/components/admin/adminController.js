ebf8f7ffd55e94eeee8dd7023971b3589b14d121
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2024-02-20 12:06:08 -0300

    Create adminController.js

---
+++ src/components/admin/adminController.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async bestProfession(req, res) {
+    console.log('Best profession');
+    return 1;
+  },
+
+  async bestClient(req, res) {
+    console.log('Best client');
+    return 1;
+  },
+};
