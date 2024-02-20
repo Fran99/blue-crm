cb5f2d5ad0951a2dc972f8a75a696273c269062f
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2024-02-20 12:41:27 -0300

    Add more tests

--- tests/integration/integration.test.js
+++ tests/integration/integration.test.js
@@ -10,11 +10,35 @@ describe('Integration Tests', () => {
   });
 
   describe('GET /contracts', () => {
-    it('Returns and array with one contract', () => request(app)
-      .get('/contracts')
-      .set('profile_id', '1')
-      .then((response) => {
-        expect(response.body[0].id).to.equal(2);
-      }));
+    it('Returns an array with one contract', async () => {
+      const response = await request(app)
+        .get('/contracts')
+        .set('profile_id', '1');
+      expect(response.body.length).to.equal(1);
+    });
+
+    it('Returns an array with two contracts', async () => {
+      const response = await request(app)
+        .get('/contracts')
+        .set('profile_id', '2');
+
+      expect(response.body.length).to.equal(2);
+    });
+  });
+
+  describe('GET /contracts/:id', () => {
+    it('Returns an object with the contract', async () => {
+      const response = await request(app)
+        .get('/contracts/1')
+        .set('profile_id', '1');
+      expect(response.body.id).to.equal(1);
+    });
+
+    it('Returns 404 since it is not authorized', async () => {
+      const response = await request(app)
+        .get('/contracts/1')
+        .set('profile_id', '2');
+      expect(response.statusCode).to.equal(404);
+    });
   });
 });
