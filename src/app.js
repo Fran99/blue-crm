40df3bdc666adf3d896900d75ed8e83d3fb5516f
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2024-02-20 08:58:53 -0300

    Add admin extra endpoints

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { Op } = require('sequelize');
 const helmet = require('helmet');
+const cors = require('cors');
 const { sequelize } = require('./model');
 const { getProfile } = require('./middleware/getProfile');
 
 const app = express();
-app.use(bodyParser.json(), helmet());
+app.use(bodyParser.json(), helmet(), cors());
 app.set('sequelize', sequelize);
 app.set('models', sequelize.models);
 
@@ -188,6 +189,40 @@ app.post('/balances/deposit/:userId', getProfile, async (req, res) => {
 
   return res.json({
     message: 'The balance has been updated',
+  });
+});
+
+// TODO: PAYMENT DATE
+
+// GET /admin/best-profession?start=<date>&end=<date> - Returns the profession that earned the most money
+// (sum of jobs paid) for any contactor that worked in the query time range.
+app.get('/admin/best-profession', async (req, res) => {
+  console.log(1);
+});
+
+app.get('/admin/profiles', async (req, res) => {
+  const { Profile } = req.app.get('models');
+  const profiles = await Profile.findAll();
+
+  return res.json({
+    data: profiles,
+  });
+});
+
+app.get('/admin/profiles/:profileId', async (req, res) => {
+  const { Profile } = req.app.get('models');
+  const profile = await Profile.findOne({
+    where: {
+      id: +req.params.profileId,
+    },
+  });
+
+  if (!profile) {
+    return res.status(404).end();
+  }
+
+  return res.json({
+    data: profile,
   });
 });
 
