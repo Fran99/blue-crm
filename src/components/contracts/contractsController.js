bce151dcf9c82c7ff815b2f94f4f13a7310ac59a
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2024-02-20 12:05:31 -0300

    Create contractsController.js

---
+++ src/components/contracts/contractsController.js
@@ -0,0 +1,49 @@
+const { Op } = require('sequelize');
+
+module.exports = {
+
+  /**
+   *
+   * @param req
+   * @param res
+   * @return {Promise<*>}
+   */
+  async findAll(req, res) {
+    const { Contract } = req.app.get('models');
+    const { id: profileId } = req.profile;
+    const contracts = await Contract.findAll({
+      where: {
+        status: {
+          [Op.not]: 'terminated',
+        },
+        [Op.and]: {
+          [req.profile.idType]: profileId,
+        },
+      },
+    });
+    return res.json(contracts);
+  },
+
+  /**
+   *
+   * @param req
+   * @param res
+   * @return {Promise<*>}
+   */
+  async findOne(req, res) {
+    const { Contract } = req.app.get('models');
+    const { id } = req.params;
+    const { id: profileId } = req.profile;
+
+    const contract = await Contract.findOne({
+      where: {
+        id,
+        [Op.and]: {
+          [req.profile.idType]: profileId,
+        },
+      },
+    });
+    if (!contract) return res.status(404).end();
+    return res.json(contract);
+  },
+};
