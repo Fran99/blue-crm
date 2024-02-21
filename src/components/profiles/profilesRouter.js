8a0b19bb3cdcf27921a0492736d585282cee0d05
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2024-02-20 23:11:11 -0300

    Update profilesRouter.js

--- src/components/profiles/profilesRouter.js
+++ src/components/profiles/profilesRouter.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { getProfile } = require('../../middleware/getProfile');
 const { findAll, findOne } = require('./profilesController');
 
 const router = express.Router();
@@ -8,13 +7,13 @@ const router = express.Router();
  * GET /profiles
  * Returns all profiles
  */
-router.get('/', getProfile, findAll);
+router.get('/', findAll);
 
 /**
  * GET /profiles/:profileId
  * Returns a single profile
  */
-router.get('/:profileId', getProfile, findOne);
+router.get('/:profileId', findOne);
 
 module.exports = {
   profilesRouter: router,
