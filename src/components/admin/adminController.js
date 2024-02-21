6d82cdffece5e545728fd8199a76e242f8759b68
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2024-02-20 23:10:43 -0300

    Add bestProfession method

--- src/components/admin/adminController.js
+++ src/components/admin/adminController.js
@@ -1,7 +1,27 @@
+const { Op, Sequelize } = require('sequelize');
+const {
+  Contract, sequelize, Job, Profile,
+} = require('../../model');
+
 module.exports = {
   async bestProfession(req, res) {
-    console.log('Best profession');
-    return 1;
+    const start = req.query.start || 0;
+    const end = req.query.end || (new Date()).toISOString();
+
+    const result = await sequelize.query(
+      `SELECT p.profession, SUM(j.price) AS totalEarned
+       FROM Profiles p
+       JOIN Contracts c ON p.id = c.ContractorId
+       JOIN Jobs j ON c.id = j.ContractId
+       WHERE j.paid = 1 
+       AND j.paymentDate IS NOT NULL
+       AND j.paymentDate BETWEEN "${start}" AND "${end}"
+       GROUP BY p.profession 
+       ORDER BY totalEarned DESC
+       LIMIT 1;`,
+    );
+
+    return res.json(result[0][0]);
   },
 
   async bestClient(req, res) {
