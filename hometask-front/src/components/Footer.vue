7390bd97b16f5014651c0476b22701ca01c91b37
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2024-02-20 23:50:25 -0300

    Update Footer.vue

--- hometask-front/src/components/Footer.vue
+++ hometask-front/src/components/Footer.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
-
+const year = (new Date()).getFullYear();
 </script>
 
 <template>
-
+  <footer>
+    <div class="container">
+      <p>&copy; {{ year }} HomeTaskFront</p>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
+footer {
+  padding: 10px 0;
+  text-align: center;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  background-color: #1c4396;
+  color: white;
+}
 
+footer p {
+  margin-bottom: 0;
+}
 </style>
