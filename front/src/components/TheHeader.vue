c06a823f2ac5a9a061ad89fece4f2637ae7ae5b9
Francisco Pepe Mantero <franciscopm1982@gmail.com>
2024-02-26 12:08:05 -0300

    Update TheHeader.vue

--- front/src/components/TheHeader.vue
+++ front/src/components/TheHeader.vue
@@ -11,7 +11,7 @@ const isHomePage = computed(() => {
 <template>
   <nav class="navbar navbar-expand-lg">
     <div class="container">
-      <router-link class="navbar-brand" to="/">HTF</router-link>
+      <router-link class="navbar-brand" to="/">BlueCRM</router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
               aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
